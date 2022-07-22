662d0ac5abbe4bc2fa6cc4622b183a88fd379832
MishK23 <70616323+MishK23@users.noreply.github.com>
2022-07-22 15:03:29 -0400

    Update FinalExecution.m

--- FinalExecution.m
+++ FinalExecution.m
@@ -3,7 +3,7 @@
 num = 1;
 finalArr = strings(length(myFiles),6);
 finalArr(1,:) = ["WellIndex","FileName","Type","Treatment","Iteration","TimePoint"];
-for j = 1:length(myFiles)
+for j = 1:10
    
    % Reading image and getting filename, which is used to make parameters   
    clf;
@@ -12,13 +12,12 @@ for j = 1:length(myFiles)
    Image = imread(current_image);
    Index = 1;
    [Image_Height,Image_Width,Number_Of_Colour_Channels] = size(Image);
-   Block_Size = 500;
+   Block_Size = floor(Image_Height/3);
    Number_Of_Blocks_Vertically = floor(Image_Height/Block_Size);
    Number_Of_Blocks_Horizontally = floor(Image_Width/Block_Size);
-   Image_Blocks = struct('Blocks',[]);
    group = extractBetween(name,11,12);
    iteration = extractBetween(name,14,15);
-   timepoint = extractBetween(name,17,30);
+   timepoint = extractBetween(name,17,34);
    
    % Setting treatment based on group
    if strcmp(group,"B2")
@@ -40,11 +39,7 @@ for j = 1:length(myFiles)
        treatment = "50 nM thapsagargin";
    end
 
-   % Main split processing and array writing
-   for Row = 1: +Block_Size: Number_Of_Blocks_Vertically*Block_Size
-     for Column = 1: +Block_Size: Number_Of_Blocks_Horizontally*Block_Size
-     
-     % Image descriptors
+   % Image descriptors
      Row_End = Row + Block_Size - 1;
      Column_End = Column + Block_Size - 1;
         
@@ -58,25 +53,21 @@ for j = 1:length(myFiles)
      
      %Pick random subimages from scan
      arr = randperm(Number_Of_Blocks_Vertically,2);
-     for z = 1:Number_Of_Blocks_Vertically
-
-       Temporary_Tile = Image(Row:Row_End,Column:Column_End,:);
-       Image_Blocks(Index).Blocks = Temporary_Tile;
-       if ismember(Index,arr)
+     for start = Block_Size: Block_Size: Image_Height-Block_Size
+       first = randi([start-Block_Size,start]);
+       Temporary_Tile = Image(first:first+Block_Size,first:first+Block_Size);
        figName = append(string(num),'.jpg');
-       subplot(Number_Of_Blocks_Vertically,Number_Of_Blocks_Horizontally,Index); imshow(Temporary_Tile);
+       imshow(Temporary_Tile);
        f = gcf;
+       set(gcf,'Color','none');
        exportgraphics(f,figName,'BackgroundColor','none');    
        numstr = string(num);
        finalArr((num+1),:) = [numstr,name,group,treatment,iteration,timepoint];
        num = num + 1;
-       end
        clf;
        Index = Index + 1;
+     end
        %Create subimages, plot them, then export plots into jpg and create
        %array of parameters describing the random subimages
-     end
-    end  
-   end
-   writematrix(finalArr,'MetaData.txt'); %Write array into text file
-end
+end 
+writematrix(finalArr,'MetaData.txt'); %Write array into text file
